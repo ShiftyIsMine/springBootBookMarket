3f79d5e1d814d3a70454eb63a18ac528f56db1ae
Shifty <jjs5827852@naver.com>
2025-03-12 11:27:02 +0900

    0312BookMarket

---
+++ src/main/java/kr/ac/kopo/shifty/springbootbookmarket/SpringBootBookMarketApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.kopo.shifty.springbootbookmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBookMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootBookMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/ac/kopo/shifty/springbootbookmarket/SpringBootBookMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.kopo.shifty.springbootbookmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBookMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
